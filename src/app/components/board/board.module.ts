94860f134c037db168d622b6ab8bf2e708c5e974
dangngocthanh <helomilo123@gmail.com>
2021-12-15 01:10:03 +0700

    asd

--- src/app/components/board/board.module.ts
+++ src/app/components/board/board.module.ts
@@ -4,22 +4,27 @@ import {BoardCreateComponent} from "./board-create/board-create.component";
 import {RouterModule, Routes} from "@angular/router";
 import {ChangePasswordComponent} from "../users/change-password/change-password.component";
 import {RegisterComponent} from "../users/register/register.component";
+import {IndexComponent} from "./index/index.component";
+import {DragDropModule} from "@angular/cdk/drag-drop";
 
 const routes: Routes = [
   {
     path:'create-board', component: BoardCreateComponent
+  },{
+    path:'index', component: IndexComponent
   },
 
 ]
 
 @NgModule({
   declarations: [
-    BoardCreateComponent
+    BoardCreateComponent,IndexComponent
   ],
 
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    DragDropModule
   ]
 })
 export class BoardModule { }
---
+++ src/app/components/board/index/index.component.ts
@@ -0,0 +1,81 @@
+import {Component, OnInit} from '@angular/core';
+import {BoardService} from "../../../services/board.service";
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from "@angular/cdk/drag-drop";
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent implements OnInit {
+  lists: any;
+  cards: any;
+  listCards: any;
+
+  constructor(private boardService: BoardService) {
+  }
+
+  ngOnInit(): void {
+    this.uploadData(3)
+  }
+
+  uploadData(id: number) {
+    this.boardService.listCard(3).subscribe(res => {
+      if (res.status == 'success') {
+        this.lists = res.lists
+        this.listCards = res.cards
+        console.log('asd')
+      }
+    })
+  }
+
+  drop(event: CdkDragDrop<any[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      for (let i = 0; i < this.listCards.length; i++) {
+        for (let j = 0; j < this.listCards[i].length; j++) {
+          if (this.listCards[i] != null) {
+            this.listCards[i][j].list_id = this.lists[i].id
+            this.listCards[i][j].seq = j
+          }
+        }
+      }
+      let data = {cards: this.listCards}
+      this.boardService.updateCard(data).subscribe(res => {
+        console.log(res)
+      })
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+      console.log(this.listCards)
+      for (let i = 0; i < this.listCards.length; i++) {
+        for (let j = 0; j < this.listCards[i].length; j++) {
+          if (this.listCards[i] != null) {
+            this.listCards[i][j].list_id = this.lists[i].id
+            this.listCards[i][j].seq = j
+          }
+        }
+      }
+      let data = {cards: this.listCards}
+      this.boardService.updateCard(data).subscribe(res => {
+        console.log(res)
+      })
+    }
+  }
+
+  drip(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.listCards, event.previousIndex, event.currentIndex);
+    moveItemInArray(this.lists, event.previousIndex, event.currentIndex);
+    for (let i = 0; i < this.lists.length; i++) {
+      this.lists[i].seq = i
+    }
+    let data = {lists: this.lists}
+    this.boardService.updateLists(data).subscribe(res => {
+    })
+  }
+
+}
