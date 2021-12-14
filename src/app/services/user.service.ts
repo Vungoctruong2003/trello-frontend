92166dbc44836a49a584f56df1f718fbf2b7e20c
dangngocthanh <helomilo123@gmail.com>
2021-12-14 14:51:58 +0700

    asd

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {environment} from 'src/environments/environment';
 
 const API_URL = `${environment.apiUrl}`;
 
@@ -10,17 +10,18 @@ const API_URL = `${environment.apiUrl}`;
 })
 export class UserService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   register(data: any): Observable<any> {
     return this.http.post<any>(API_URL + '/auth/register', data);
   }
 
-  changePassword(data: any): Observable<any>{
-    let token = localStorage.getItem('access_token')
+  changePassword(data: any): Observable<any> {
+    let token = localStorage.getItem('access_token');
     let header = {
-      headers: new HttpHeaders().set("Authorization",`Bearer ${token}`)
-    }
-    return this.http.post<any>(API_URL + '/auth/change-pass',data,header);
+      headers: new HttpHeaders().set("Authorization", `Bearer ${token}`)
+    };
+    return this.http.post<any>(API_URL + '/auth/change-pass', data, header);
   }
 }
