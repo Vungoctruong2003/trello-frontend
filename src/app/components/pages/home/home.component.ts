3941486ceaeee3c9a5b10a5f0d55b80206669e67
dangngocthanh <helomilo123@gmail.com>
2021-12-22 12:03:42 +0700

    push cuoi

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -129,8 +129,8 @@ export class HomeComponent implements OnInit {
       cancelButtonText: 'Hủy'
     }).then((result) => {
       if (result.value) {
-        if (role == 1 || role == 2) {
-          this.groupService.delete(id).subscribe(res => {
+        if (role == 1) {
+          this.groupService.deleteGroup(id).subscribe(res => {
             console.log(res)
             this.router.navigate(['/load-home'])
           })
--- src/app/services/group.service.ts
+++ src/app/services/group.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {environment} from 'src/environments/environment';
 
 const API_URL = `${environment.apiUrl}`;
 
@@ -11,64 +11,75 @@ const API_URL = `${environment.apiUrl}`;
 export class GroupService {
 
   userGroupId: any
+
   constructor(
     private http: HttpClient,
-  ) { }
-
-  createGroup(data: any): Observable<any>{
-    let token = localStorage.getItem('access_token')
-    let header = {
-      headers: new HttpHeaders().set("Authorization",`Bearer ${token}`)
-    }
-    return this.http.post<any>(API_URL + '/group/store', data,header);
+  ) {
   }
 
-  addUserToGroup(data: any): Observable<any>{
+  createGroup(data: any): Observable<any> {
     let token = localStorage.getItem('access_token')
     let header = {
-      headers: new HttpHeaders().set("Authorization",`Bearer ${token}`)
+      headers: new HttpHeaders().set("Authorization", `Bearer ${token}`)
     }
-    return this.http.post<any>(API_URL + '/group/addUser', data,header);
+    return this.http.post<any>(API_URL + '/group/store', data, header);
   }
 
-  getUser(id: number): Observable<any>{
+  addUserToGroup(data: any): Observable<any> {
     let token = localStorage.getItem('access_token')
     let header = {
-      headers: new HttpHeaders().set("Authorization",`Bearer ${token}`)
+      headers: new HttpHeaders().set("Authorization", `Bearer ${token}`)
     }
-    return this.http.get<any>(API_URL + '/group/getUser/'+ id,header);
+    return this.http.post<any>(API_URL + '/group/addUser', data, header);
   }
 
-  changeRole(data:any, id:any):Observable<any>{
+  getUser(id: number): Observable<any> {
     let token = localStorage.getItem('access_token')
     let header = {
-      headers: new HttpHeaders().set("Authorization",`Bearer ${token}`)
+      headers: new HttpHeaders().set("Authorization", `Bearer ${token}`)
     }
-    return this.http.put<any>(API_URL + '/group/changeRole/'+ id,data,header);
+    return this.http.get<any>(API_URL + '/group/getUser/' + id, header);
   }
 
-  getRole(id: any): Observable<any>{
+  changeRole(data: any, id: any): Observable<any> {
     let token = localStorage.getItem('access_token')
     let header = {
-      headers: new HttpHeaders().set("Authorization",`Bearer ${token}`)
+      headers: new HttpHeaders().set("Authorization", `Bearer ${token}`)
     }
-    return this.http.get<any>(API_URL + '/group/getRole/'+ id,header);
+    return this.http.put<any>(API_URL + '/group/changeRole/' + id, data, header);
   }
 
-  delete(data:any):Observable<any>{
+  getRole(id: any): Observable<any> {
     let token = localStorage.getItem('access_token')
     let header = {
-      headers: new HttpHeaders().set("Authorization",`Bearer ${token}`)
+      headers: new HttpHeaders().set("Authorization", `Bearer ${token}`)
     }
-    return this.http.post<any>(API_URL + '/group/delete' ,data,header);
+    return this.http.get<any>(API_URL + '/group/getRole/' + id, header);
   }
 
-  outGroup(id:any):Observable<any>{
+  delete(data: any): Observable<any> {
     let token = localStorage.getItem('access_token')
     let header = {
-      headers: new HttpHeaders().set("Authorization",`Bearer ${token}`)
+      headers: new HttpHeaders().set("Authorization", `Bearer ${token}`)
     }
-    return this.http.get<any>(API_URL + '/group/outGroup/'+ id,header);
+    return this.http.post<any>(API_URL + '/group/delete', data, header);
   }
+
+  deleteGroup(id: any): Observable<any> {
+    let token = localStorage.getItem('access_token')
+    let header = {
+      headers: new HttpHeaders().set("Authorization", `Bearer ${token}`)
+    }
+    return this.http.get<any>(API_URL + '/group/deleteGroup/' + id, header);
+  }
+
+  outGroup(id: any): Observable<any> {
+    let token = localStorage.getItem('access_token')
+    let header = {
+      headers: new HttpHeaders().set("Authorization", `Bearer ${token}`)
+    }
+    return this.http.get<any>(API_URL + '/group/outGroup/' + id, header);
+  }
+
 
 }
